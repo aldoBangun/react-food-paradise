6f13e4e8b0c40a0edfad0389f4785cac110c42f3
aldoBangun <punk.aldo68@gmail.com>
2022-07-11 02:41:56 +0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import RecipeAdd from './pages/recipe/RecipeAdd'
+import ProfileDetail from './pages/profile/ProfileDetail'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
     <>
-      <h1> Hello World </h1>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path="recipes/add" element={<RecipeAdd />} />
+          <Route path="profile" element={<ProfileDetail />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
