b608f4b8ac740b450da1c335331066b762686507
aldoBangun <punk.aldo68@gmail.com>
2022-07-12 13:20:05 +0800

    Initiate NotFound Page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap'
+
+export default function NotFound() {
+  return (
+    <>
+      <Container className="h-section">
+        <h1>Page not Found</h1>
+      </Container>
+    </>
+  )
+}
