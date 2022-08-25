e94d13ef10b81464945a3a27fc7f59bb93da4b79
aldoBangun <punk.aldo68@gmail.com>
2022-08-25 19:38:08 +0800

    modify routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
 import RecipeAdd from './pages/recipe/RecipeAdd'
 import ProfileDetail from './pages/profile/ProfileDetail'
 import NotFound from './pages/NotFound'
+import AuthLayout from './components/layout/AuthLayout'
+import AppLayout from './components/layout/AppLayout'
+import Login from './pages/auth/Login'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path="recipes/add" element={<RecipeAdd />} />
-          <Route path="profile" element={<ProfileDetail />} />
-          <Route path='*' element={<NotFound />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />}/>
+          </Route>
+          <Route element={<AppLayout />}>
+            <Route path='/' element={<Landing />} />
+            <Route path="recipes/add" element={<RecipeAdd />} />
+            <Route path="profile" element={<ProfileDetail />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   )
